1af10e922f203cb9b4b3f9ff61b03c9c151e4d15
Kim-Ly Le <kle@pmc.int>
2020-02-18 10:26:02 +0100

    Implemented http calls to inmemorydata

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { Observable } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-heroes',
@@ -24,8 +25,22 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
   getHeroes(): void {
-    this.heroService.getHeroes() 
+    this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
   }
 
